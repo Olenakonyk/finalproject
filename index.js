6e579d66b72c881bf6b27ededc95c9688de7c21d
Olhakonyk <olhakonyk@gmail.com>
2016-06-13 11:19:04 +0300

    https://finalproject-konyk.c9users.io/

---
+++ index.js
@@ -0,0 +1,53 @@
+(function() {
+
+  'use strict';
+
+  // define variables
+  var items = document.querySelectorAll(".timeline li");
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+
+})();
+
+
+$('#btn-send').click(function(e) {
+  $.ajax({
+      url: "https://formspree.io/olhakonyk@gmail.com",  
+      method: "POST",
+      error: function () {
+        $('#container').html('<h1>Error!</h1>')
+      },
+      data: {
+             email: email.value,
+             name: name.value,
+             message: msg.value
+             },
+      dataType: "json"
+  }).done(function() {
+     $('#container').html('<h1>Thank you!</h1>')
+  } );
+  e.preventDefault();
+});
